5060882d869912d07b0bd4a1104171284d3a71e1
Gustavo <lucenagustavo404@gmail.com>
2023-05-15 13:01:26 -0300

    encryptation atualization with encryptSalt

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+// require('dotenv').config()
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -9,7 +11,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB")
@@ -21,57 +23,64 @@ const userSchema = new mongoose.Schema({
 
 //this is the key that the encryption use to encrypt
 
-                                //this is the enviroment 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
+//this is the enviroment
+// userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
+
+//i trade this for another more security methodo
 const User = new mongoose.model("User", userSchema);
 
 
 
 app.get("/", function (req, res) {
-    res.render("home")    
+    res.render("home")
 });
 
 //login
 app.route("/login")
-.get(function (req, res) {
-    res.render("login")    
-})
-.post(function(req,res) {
-    console.log(req.body.username)
-    User.findOne({email: req.body.username}).then(function(foundUser) {
-        console.log(foundUser)
-        if(req.body.password === foundUser.password){
-            res.render("secrets")
-        }else{
-            res.send("user 404")
-        }
-    })    
-})
+    .get(function (req, res) {
+        res.render("login", { Title: "" })
+    })
+    .post(function (req, res) {
+
+        console.log(req.body.username)
+        User.findOne({ email: req.body.username }).then(function (foundUser) {
+            console.log(foundUser)
+
+            bcrypt.compare(req.body.password, foundUser.password).then(function (result) {
+                if (result === true) { res.render("secrets") } else {
+
+                    res.render("login", { Title: "notFound" })
+                }
+            });
+        });
+    });
 
 
 //register
 app.route("/register")
-.get(function (req, res) {
-    res.render("register")    
-})
-.post(function(req, res){
-    const user = new User ({
-        email: req.body.username,
-        password: req.body.password
-    });
-    user.save().then(function(suscess){
-        if(suscess){
-            res.send("suscess");
-        }else{
-            res.send("error 404")
-        }
+    .get(function (req, res) {
+        res.render("register")
     })
-})
+    .post(function (req, res) {
+        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+            const user = new User({
+                email: req.body.username,
+                password: hash
+            });
+            user.save().then(function (suscess) {
+                if (suscess) {
+                    res.render("secrets")
+                } else {
+                    res.send("error 404")
+                }
+            })
+        });
+    })
 
 
 
 
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Working on port 3000");
 })
